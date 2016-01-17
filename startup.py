c7da04fae8e6f67c2ae049654968e5c94056d482
NizarBoussarsar <nizar.boussarsar@esprit.tn>
2016-01-17 01:30:36 +0100

    [DONE] Working on sun & moon light

--- startup.py
+++ startup.py
@@ -1,83 +1,95 @@
-from datetime import tzinfo
-
 import requests
 import time
 import os
 import datetime
 import dateutil.parser
+import json
+import warnings
+
+warnings.filterwarnings("ignore", category=UnicodeWarning)
 
 # import RPi.GPIO as GPIO
 
 # code = os.environ["TANKCODE"]
-code = "354"
-json_obj = ''
-lat = '43.64152'
-lng = '7.009186'
+code = '354'
+json_obj = {}
 
 
 # ledPin = 23
 
-def getTankInfos():
+def getTankInfos(code):
     req = requests.get("https://aqua-ocs.herokuapp.com/tank?code=" + code)
-    json_obj = req.json()
-    json_obj = json_obj[0]
+    local_json_obj = req.json()
+    local_json_obj = local_json_obj[0]
+    print local_json_obj
+    return local_json_obj
 
 
-# print json_obj
-
-def updateTankStatus():
-    payload = {'state': 'online'}
-    requests.put("https://aqua-ocs.herokuapp.com/tank/" + str(json_obj['id']), data=payload)
+def updateTankStatus(global_json_obj):
+    print requests.put("https://aqua-ocs.herokuapp.com/tank/" + str(global_json_obj['id']), data="{'state': 'online'}").status_code
 
 
-def getBrightnessLevel():
-    # https://aqua-ocs.herokuapp.com/tank/light?lat=&lng= hedha yrajaalek moon value {"moon":0.46980866740073773,"status":"OK"}
-    req = requests.get("https://aqua-ocs.herokuapp.com/tank/light?lng=" + lng + "&lat=" + lat)
-    json_obj = req.json()
-    # print json_obj
+def getBrightnessLevel(global_json_obj):
+    # https://aqua-ocs.herokuapp.com/tank/light?lat=&lng= hedha yrajaalek moon value moon 0.46980866740073773 status OK"}
+    req = requests.get("https://aqua-ocs.herokuapp.com/tank/light?lng=" + str(global_json_obj['longitude']) + "&lat=" + str(global_json_obj['latitude']))
+    local_json_obj = req.json()
 
     now = datetime.datetime.now().replace(tzinfo=None)
-    sunriseTime = dateutil.parser.parse(json_obj['lightStart']).replace(tzinfo=None)
-    sunsetTime = dateutil.parser.parse(json_obj['lightEnd']).replace(tzinfo=None)
-    solarNoonTime = dateutil.parser.parse(json_obj['lightMid']).replace(tzinfo=None)
-    lightLength = json_obj['lightLength']
+    sunriseTime = dateutil.parser.parse(local_json_obj['lightStart']).replace(tzinfo=None)
+    sunsetTime = dateutil.parser.parse(local_json_obj['lightEnd']).replace(tzinfo=None)
+    solarNoonTime = dateutil.parser.parse(local_json_obj['lightMid']).replace(tzinfo=None)
+    lightLength = local_json_obj['lightLength']
+    moonBrightness = local_json_obj['moon']
+    val = 0
+    data = {}
 
-    ##
+    '''
+    now = datetime.datetime.now().replace(tzinfo=None)
+    sunriseTime = dateutil.parser.parse('2016-01-16T07:01:42+00:00').replace(tzinfo=None)
+    sunsetTime = dateutil.parser.parse('2016-01-16T16:21:42+00:00').replace(tzinfo=None)
+    solarNoonTime = dateutil.parser.parse('2016-01-16T11:41:42+00:00').replace(tzinfo=None)
+    lightLength = 33600
+    moonBrightness = 0.48616002612055303
+    val = 0
+    data = {}
+    '''
+
     if (now > sunriseTime) and (now < sunsetTime):
-        print "morning"
+        data['time'] = 'sun'
         if now < solarNoonTime:
-            print "9bal"
             seconds = time.mktime(solarNoonTime.timetuple()) - time.mktime(now.timetuple())
-            val = int(100) * float((seconds / lightLength))
-            print "val: " + str("%.2f" % val)
         else:
-            print "baad"
             seconds = time.mktime(now.timetuple()) - time.mktime(solarNoonTime.timetuple())
-            val = int(100) * float((seconds / lightLength))
-            print "val: " + str("%.2f" % val)
+        val = 1 - (float((seconds / (lightLength / 2))))
+        val = float(("%.3f" % float(val))) * int(100)
     else:
-        print "night"
-        val = float(("%.2f" % json_obj['moon'])) * int(100)
-        print "Brightness level: " + str(val)
-    ##
+        data['time'] = 'moon'
+        val = float(("%.3f" % float(moonBrightness))) * int(100)
+
+    data['brightness'] = val
+    json_data = json.dumps(data)
+    print json_data
+    return json_data
+
 
 def start():
-    if (json_obj != []):
-        getTankInfos()
-        updateTankStatus()
+    json_obj = getTankInfos(code)
+    if json_obj != []:
+        updateTankStatus(json_obj)
+        json_brightness = getBrightnessLevel(json_obj)
     else:
-        '''
-		for i in range(0, 4):
-			GPIO.output(ledPin, GPIO.LOW)
-			print "BLINK"
-			time.sleep(1)
-			GPIO.output(ledPin, GPIO.HIGH)
-			print "BLANK"
-			time.sleep(1)
-		'''
+        for i in range(0, 4):
+            #GPIO.output(ledPin, GPIO.LOW)
+            print "BLINK"
+            time.sleep(1)
+            #GPIO.output(ledPin, GPIO.HIGH)
+            print "BLANK"
+            time.sleep(1)
         time.sleep(10)
         # time.sleep(300)
+        #I think when using a environment variable, it is required to rerun the script in another session
         start(json_obj)
+        #
 
 
 '''
@@ -86,6 +98,4 @@ GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
 GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
 '''
 
-# getTankInfos()
-# start()
-getBrightnessLevel()
+start()
