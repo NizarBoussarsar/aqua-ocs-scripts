03e173953df07daea28ed21c09781c6fafe1b204
Raedslab <raed@riseup.net>
2016-01-18 19:49:49 +0100

    added ping client

---
+++ ping.py
@@ -0,0 +1,16 @@
+import requests
+import warnings
+
+warnings.filterwarnings("ignore", category=UnicodeWarning)
+
+# code = os.environ["TANKCODE"]
+code = '354'
+
+
+def pingServer(code):
+    req = requests.get("https://aqua-ocs.herokuapp.com/tank/ping?tankCode=" + code)
+    local_json_obj = req.json()
+    return local_json_obj["status"]
+
+
+pingServer(code)
